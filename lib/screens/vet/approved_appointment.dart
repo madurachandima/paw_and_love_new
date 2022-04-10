4afcadcbcb2377177cb820c2f1222373affdde21
MaduraDiasTreinetic <102131928+MaduraDiasTreinetic@users.noreply.github.com>
2022-04-10 16:56:24 +0530

    Update approved_appointment.dart

--- lib/screens/vet/approved_appointment.dart
+++ lib/screens/vet/approved_appointment.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:paw_and_love/screens/vet/appointmens_tabs/vet_approved_appointments.dart';
 import 'package:paw_and_love/screens/vet/appointmens_tabs/vet_completed_appointments.dart';
+import 'package:sizer/sizer.dart';
 
 class ApprovedAppointment extends StatelessWidget {
   const ApprovedAppointment({Key? key}) : super(key: key);
@@ -14,10 +15,14 @@ class ApprovedAppointment extends StatelessWidget {
         length: 2,
         child: Scaffold(
           appBar: AppBar(
-            bottom: const TabBar(
+            bottom:  TabBar(
+              labelColor: Colors.white,
+              indicatorColor: Colors.white,
+              indicatorSize: TabBarIndicatorSize.label,
+              unselectedLabelColor: Colors.black,
               tabs: [
-                Tab(child: Text("Approved")),
-                Tab(child: Text("Completed")),
+                Tab(child: Text("Approved",style: TextStyle(fontSize: 15.sp),)),
+                Tab(child: Text("Completed",style: TextStyle(fontSize: 15.sp))),
               ],
             ),
             // title: const Text('Tabs Demo'),
